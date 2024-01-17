55ee3b50bf0d05d37d0bae7d20025d7e09f090e6
Dubakula Sai Venkata Chaitanya <113052920+Atan-D-RP4@users.noreply.github.com>
2024-01-17 08:27:19 +0530

    Wed Jan 17 08:27:10 IST 2024

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main(void)
 {
     printf("Hello, First Repository\n");
     printf("Experimneting with GitLens");
+    printf("What does '#' do?: %#s\n", "...?");
 }
