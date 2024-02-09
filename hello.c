13e9425641c0ecb4780e8a88d55536b5da0a4c72
Atan-D-RP4 <chaitanyarp4@gmail.com>
2024-02-10 00:50:54 +0530

    9th Of February 2024

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-int main(void)
-{
-    printf("Hello, First Repository\n");
-    printf("Experimneting with GitLens");
-    printf("What does '#' do?: %#s\n", "...?");
-}
